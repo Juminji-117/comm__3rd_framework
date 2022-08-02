ef8f138f2ee16fb07ce4ad0f3f4ebf72ae199aee
Ju, Minji <alswl0w0@gmail.com>
2022-08-02 14:27:13 +0900

    AppTest : ArticleService를 Container에서 얻는 TestCase 추가

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,6 +2,7 @@ package com.ll.exam;
 
 import com.ll.exam.home.controller.HomeController;
 import com.ll.exam.article.controller.ArticleController;
+import com.ll.exam.article.service.ArticleService;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -52,5 +53,20 @@ public class AppTest {
 
         assertThat(names).contains("home");
         assertThat(names).contains("article");
+    }
+
+    @Test
+    public void ioc__articleService() {
+        ArticleService articleService = Container.getObj(ArticleService.class);
+
+        assertThat(articleService).isNotNull();
+    }
+
+    @Test
+    public void ioc__articleService__싱글톤() {
+        ArticleService articleService1 = Container.getObj(ArticleService.class);
+        ArticleService articleService2 = Container.getObj(ArticleService.class);
+
+        assertThat(articleService2).isEqualTo(articleService1);
     }
 }
