ef984a1f997223ff5080e540661ad07038a41e82
Ju, Minji <alswl0w0@gmail.com>
2022-08-02 13:48:42 +0900

    HomeController TestCase 추가

--- src/main/java/com/ll/exam/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/home/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.ll.exam.home.controller;
 
+import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    public HomeController() {
+    }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,5 +1,6 @@
 package com.ll.exam;
 
+import com.ll.exam.home.controller.HomeController;
 import com.ll.exam.article.controller.ArticleController;
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +29,21 @@ public class AppTest {
         ArticleController articleController2 = Container.getArticleController();
 
         assertThat(articleController2).isEqualTo(articleController1);
+    }
+
+    @Test
+    public void ioc__homeController() {
+        HomeController homeController = Container.getHomeController();
+
+        assertThat(homeController).isNotNull();
+    }
+
+    @Test
+    public void ioc__homeController__싱글톤() {
+        HomeController homeController1 = Container.getHomeController();
+        HomeController homeController2 = Container.getHomeController();
+
+        assertThat(homeController2).isEqualTo(homeController1);
     }
 
     @Test
