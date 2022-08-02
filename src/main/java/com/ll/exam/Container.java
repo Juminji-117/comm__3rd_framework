250275ac7c4ea3ee26d772760f4669818ae87341
Ju, Minji <alswl0w0@gmail.com>
2022-08-02 15:18:20 +0900

    Service도 Component로 지정하여 스캔

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
+
 import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.Service;
 import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.home.controller.HomeController;
 import org.reflections.Reflections;
@@ -11,27 +13,40 @@ public class Container {
     private static Map<Class, Object> objects;
     static {
         objects = new HashMap<>();
-        //아래 getControllerNames()에서 했던 구현과 유사
+
+        scanComponents();
+    }
+
+    private static void scanComponents() {
+        scanServices();
+        scanControllers();
+    }
+
+    private static void scanServices() {
+        Reflections ref = new Reflections("com.ll.exam");
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Service.class)) {
+            objects.put(cls, Ut.cls.newObj(cls, null));
+        }
+    }
+
+    private static void scanControllers() {
         Reflections ref = new Reflections("com.ll.exam");
         for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
             objects.put(cls, Ut.cls.newObj(cls, null));
         }
     }
-
-
-        public static <T> T getObj(Class<T> cls) {
-            return (T)objects.get(cls);
-        }
-
-        public static List<String> getControllerNames() {
-            List<String> names = new ArrayList<>();
-            Reflections ref = new Reflections("com.ll.exam");
-            for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
-                String name = cls.getSimpleName(); // HomeController
-                name = name.replace("Controller", ""); // Home
-                name = Ut.str.decapitalize(name); // home
-                names.add(name.replace("Controller", name));
-            }
-            return names;
-        }
+    public static <T> T getObj(Class<T> cls) {
+        return (T)objects.get(cls);
     }
+    public static List<String> getControllerNames() {
+        List<String> names = new ArrayList<>();
+        Reflections ref = new Reflections("com.ll.exam");
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
+            String name = cls.getSimpleName(); // HomeController
+            name = name.replace("Controller", ""); // Home
+            name = Ut.str.decapitalize(name); // home
+            names.add(name.replace("Controller", name));
+        }
+        return names;
+    }
+}
