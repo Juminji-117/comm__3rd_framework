10498c2a7a43639ba66740a7316a0715989c919a
Ju, Minji <alswl0w0@gmail.com>
2022-08-02 13:55:29 +0900

    Container : new없이 객체 생성->Ut.cls.newObj()로

--- src/main/java/com/ll/exam/annotation/Autowired.java
+++
@@ -1,6 +0,0 @@
-package com.ll.exam.annotation;
-
-// @interface 어노테이션 정의
-public @interface Autowired {
-
-}
--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -1,13 +1,11 @@
 package com.ll.exam.article.controller;
 
-import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.GetMapping;
 
 @Controller // ArticleController 가 컨트롤러 이다.
 // 아래 ArticleController 클래스는 Controller 이다.
 public class ArticleController {
-    @Autowired
     public ArticleController() {
     }
     @GetMapping("/usr/article/list") // /usr/article/list/free 와 같이 관련된 요청을 처리하는 함수이다.
